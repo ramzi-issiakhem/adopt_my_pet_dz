3597a08911db3327559e9b55c39f7b2f3fbbc440
Ramzi Issiakhem <Disc_ord@outlook.fr>
2020-12-12 16:20:27 +0100

    BackUp Commit

--- lib/features/Lost/presentation/pages/lost_page.dart
+++ lib/features/Lost/presentation/pages/lost_page.dart
@@ -1,14 +1,27 @@
-class FindPage extends StatelessWidget {
+import 'package:adopt_my_pet_dz/core/shared/app_bar.dart';
+import 'package:adopt_my_pet_dz/core/shared/sizes.dart';
+import 'package:flutter/material.dart';
+
+class LostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    globalSizeClass.recalcuate(MediaQuery.of(context));
+
     return MaterialApp(
       home: Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(globalSizeClass.heightTopNavBar),
           child: MainAppBar().call(context),
         ),
-        body: BuildBlocInit(),
+        body: BuildBody(),
       ),
     );
+  }
+}
+
+class BuildBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
